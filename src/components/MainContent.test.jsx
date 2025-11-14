54c934757bd4e6ccc7e84fd182a6b28e338de638
mrperfumo_indra <mrperfumo@minsait.com>
2025-11-14 10:25:03 +0100

    Modificamos triggers

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -11,7 +11,7 @@ describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
 
-    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole('button')).not.toBeInTheDocument(); // Cambiado para que falle
   });
 
   it('should show the help area after clicking the button', async () => {
